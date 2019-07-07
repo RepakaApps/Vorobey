14b94de6e564fe96ff4426935e9a241fb9bf7e22
RepakaApps <repakaapps@gmail.com>
2019-07-07 12:46:08 +0500

    Games

---
+++ Assets/Scripts/ScoreSave.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSave : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Text>().text = PlayerPrefs.GetInt("Score").ToString();
+    }
+}
